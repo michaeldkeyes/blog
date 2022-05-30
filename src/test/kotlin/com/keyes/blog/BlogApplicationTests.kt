ef175a69e81b83d711190f94db30081a2f070c24
Mike <michael.d.keyes@gmail.com>
2022-05-30 15:19:42 -0400

    Initial setup

---
+++ src/test/kotlin/com/keyes/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.keyes.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
